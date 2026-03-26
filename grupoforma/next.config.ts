9f7c23f882747119217026ec0caba7975f7091b2
Guilherme Padial <guilhermepadial.4@gmail.com>
2026-03-26 08:27:52 -0300

    Update next config

--- grupoforma/next.config.ts
+++ grupoforma/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Isso avisa o Next.js para gerar arquivos estáticos (HTML/CSS/JS)
+  output: "export",
+
+  // Como a Locaweb não tem o servidor do Next.js para otimizar imagens,
+  // precisamos desativar essa função para não dar erro no build
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
